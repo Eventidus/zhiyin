5df63e347794ef4a6abb8e18e5bd7d952fed9a09
huanghu <iamzixiang@163.com>
2023-02-11 23:03:51 +0800

    Daily Submssion

--- src/views/SongRec.vue
+++ src/views/SongRec.vue
@@ -14,6 +14,71 @@
         <p>不管将会面对什么样的结局</p>
       </div>
     </div>
+    <div class="container">
+      <div class="option">
+        <img src="../assets/images/songRec/playback.png" alt="" />
+        <span>全部播放</span>
+        <img src="../assets/images/songRec/more.png" alt="" />
+      </div>
+      <div class="list">
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+        <div class="list-item">
+          <img src="../assets/images/songRec/bigCover.png" alt="" />
+          <div class="detail">
+            <span>漂洋过海来看你</span>
+            <span>周深</span>
+          </div>
+          <img src="../assets/images/songRec/love.png" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +90,8 @@ export default {};
 .songRec {
   padding-top: 9.07vw;
   background-image: url(../assets/images/songRec/bg.png);
+  background-repeat: no-repeat;
+  background-size: cover;
   padding-left: 4.53vw;
   padding-right: 4.53vw;
   header {
@@ -80,6 +147,69 @@ export default {};
           font-size: 3.2vw;
           letter-spacing: 0.08vw;
           margin-top: 1.73vw;
+        }
+      }
+    }
+  }
+  .container {
+    margin-top: 14vw;
+    .option {
+      img:first-child {
+        width: 0.43rem;
+        height: 0.43rem;
+      }
+      img:last-child {
+        width: 0.08rem;
+        height: 0.4rem;
+        position: absolute;
+        right: 6vw;
+      }
+      span {
+        color: #fff;
+        margin-left: 1.73vw;
+        font-family: SourceHanSansCN-Normal;
+        font-size: 3.73vw;
+      }
+    }
+    .list {
+      margin-top: 4.4vw;
+
+      .list-item {
+        margin-bottom: 1.33vw;
+        height: 14.93vw;
+        display: flex;
+        align-items: center;
+        img:first-child {
+          width: 14.67vw;
+          height: 14.67vw;
+          background-color: #ffffff;
+          border-radius: 0.67vw;
+        }
+        img:last-child {
+          width: 4.4vw;
+          height: 3.6vw;
+          position: absolute;
+          right: 4vw;
+        }
+        .detail {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          margin-left: 2.53vw;
+          margin-top: 0;
+          span:first-child {
+            font-family: SourceHanSansCN-Regular;
+            font-size: 4vw;
+            letter-spacing: 0.03vw;
+            color: #ffffff;
+          }
+          span:last-child {
+            font-family: SourceHanSansCN-Normal;
+            font-size: 3.47vw;
+            margin-top: 2.13vw;
+            letter-spacing: 0.09vw;
+            color: #979797;
+          }
         }
       }
     }
