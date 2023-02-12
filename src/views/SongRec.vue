d9b1a03f27bfb4d58dadcca11b18aa2b946eebf7
huanghu <iamzixiang@163.com>
2023-02-12 23:45:30 +0800

    Daily Submssion

--- src/views/SongRec.vue
+++ src/views/SongRec.vue
@@ -79,6 +79,17 @@
         </div>
       </div>
     </div>
+    <div class="top">
+      <div class="light">
+        <img src="../assets/images/records.png" alt="" class="record" />
+        <img src="../assets/images/recordCover.png" alt="" class="cover" />
+      </div>
+      <p>漂洋过海来看你</p>
+      <div class="right">
+        <img src="../assets/images/stop.png" alt="" class="play" />
+        <img src="../assets/images/playlist.png" alt="" class="list" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -211,6 +222,68 @@ export default {};
             color: #979797;
           }
         }
+      }
+    }
+  }
+  .top {
+    margin-top: 4.8vw;
+    padding-bottom: 6.27vw;
+    height: 1.05rem;
+    display: flex;
+    background-image: url(../assets/images/top-front.png);
+    background-size: 9.11rem 0.93rem;
+    background-repeat: no-repeat;
+    background-position: center 0.12rem;
+    position: relative;
+    .light {
+      .record {
+        position: relative;
+        bottom: 0.25rem;
+        width: 11.6vw;
+        height: 11.6vw;
+        margin-left: 3.67vw;
+        // animation: rotate 2s linear infinite;
+
+        // @keyframes rotate {
+        //   0% {
+        //     transform: rotate(0deg);
+        //   }
+        //   100% {
+        //     transform: rotate(360deg);
+        //   }
+        // }
+      }
+      .cover {
+        width: 8.4vw;
+        height: 8.4vw;
+        position: relative;
+        left: -10.15vw;
+        top: -4.05vw;
+      }
+    }
+    p {
+      font-family: MiSans;
+      font-size: 3.2vw;
+      color: #dcdddf;
+      position: relative;
+      left: -3.2vw;
+    }
+    .right {
+      position: absolute;
+      right: 8vw;
+      vertical-align: baseline;
+      .play {
+        position: relative;
+        top: 2.13vw;
+        width: 7.47vw;
+        height: 7.47vw;
+        margin-right: 3.47vw;
+      }
+      .list {
+        position: relative;
+        top: 0.5vw;
+        width: 5.47vw;
+        height: 4.67vw;
       }
     }
   }
